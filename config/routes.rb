c01e5e9800d6ca52e785fda773b0859b08fac013
Ahmad Arif <ahmadarif@Ahmads-MacBook-Air.local>
2023-05-02 23:30:20 +0300

    Add procfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root "application#home"
-  post '/episodes', to: 'episodes#create'
-  get '/episodes/new', to: 'episodes#new'
   resources :episodes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
