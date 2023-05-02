0fdc883c21d081d121664c5c366027326fc0bb0f
Ahmad Arif <ahmadarif@Ahmads-MacBook-Air.local>
2023-05-02 22:23:19 +0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "application#home"
+  post '/episodes', to: 'episodes#create'
+  get '/episodes/new', to: 'episodes#new'
   resources :episodes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
